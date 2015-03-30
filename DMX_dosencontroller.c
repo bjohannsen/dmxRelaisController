e9cccd3af22bf51a1e15fbe6057ab8edec924d72
dreimalb <mail.bjo@gmx.de>
2015-03-30 22:21:34 +0200

    Initial commit of project.

---
+++ DMX_dosencontroller.c
@@ -0,0 +1,303 @@
+#include "DMX_dosencontroller.h"
+
+#define DMX_CHANNEL 45
+#define DMX_NUM_CHANNELS 1
+#define LED_TIME 50000
+#define SW_CNT_MAX 1000
+
+volatile uint16_t _led_latency = LED_TIME;
+volatile uint8_t _dmx_old_data[DMX_NUM_CHANNELS];
+volatile uint8_t _dmx_data[DMX_NUM_CHANNELS];
+volatile relais_data_t _relais_data[NUM_RELAIS];
+volatile uint8_t _strobe_count[NUM_RELAIS];
+volatile uint8_t _flag = 0;
+
+ISR(TIMER1_COMPA_vect)
+{
+	static uint8_t pwm_cnt = 0;
+	uint8_t i = 0;
+
+	OCR1A += (uint16_t) T_PWM;
+
+	for (i = 0; i < NUM_RELAIS; i++)
+	{
+		if (_relais_data[i].pwm > pwm_cnt)
+		{
+			if (_relais_data[i].enable)
+			{
+				if (_relais_data[i].strobe > STROBE_OFF)
+				{
+					_relais_data[i].enable--;
+				}
+				R_PORT |= _relais_data[i].PIN;
+			}
+			else
+			{
+				R_PORT &= ~(_relais_data[i].PIN);
+			}
+		}
+		else
+		{
+			R_PORT &= ~(_relais_data[i].PIN);
+		}
+	}
+
+	if (pwm_cnt >= (uint8_t)(PWM_STEPS - 1))
+	{
+		for (i = 0; i < NUM_RELAIS; i++)
+		{
+			if (_relais_data[i].strobe > 0)
+			{
+				if (_strobe_count[i] == 0 )
+				{
+					_strobe_count[i] = _relais_data[i].strobe;
+					_relais_data[i].enable = FLASH_CNT;
+				}
+				else
+				{
+					_strobe_count[i]--;
+				}
+			}
+			else
+			{
+				_relais_data[i].enable = 1;
+				_strobe_count[i] = 0;
+			}
+		}
+		pwm_cnt = 0;
+	}
+	else
+	{
+		pwm_cnt++;
+	}
+}
+
+ISR(USART_RX_vect)
+{
+	static uint16_t channel;
+	static uint8_t channel_num;
+	uint8_t data = UDR;
+
+	if (UCSRA & (1 << FE))
+	{
+		if (data == 0)
+		{
+			channel = 1;
+		}
+		else
+		{
+			channel++;
+		}
+		UCSRA &= ~(1 << FE);
+	}
+	else
+	{
+		if (channel == DMX_CHANNEL)
+		{
+			_led_latency = LED_TIME;
+			channel_num = 0;
+			_dmx_data[channel_num++] = data;
+			_flag = 1;
+		}
+		else if (channel_num > DMX_NUM_CHANNELS)
+		{
+			_dmx_data[channel_num++] = data;
+			_flag = 1;
+		}
+		channel++;
+	}
+}
+
+
+void processData()
+{
+	uint8_t tmp = 0;
+	uint8_t i = 0;
+	for (i = 0; i < DMX_NUM_CHANNELS; i++)
+	{
+		if (_dmx_data[i] != _dmx_old_data[i])
+		{
+			if (i == 0)
+			{
+				if (_dmx_data[i] & D1_POWER)
+				{
+					if ((_dmx_data[i] & D1_POWER_100) == D1_POWER_100)
+					{
+						tmp = PWM_FULL;
+					}
+					else if ((_dmx_data[i] & D1_POWER_60) == D1_POWER_60)
+					{
+						tmp = PWM_HALF;
+					}
+					else if ((_dmx_data[i] & D1_POWER_30) == D1_POWER_30)
+					{
+						tmp = PWM_MIN;
+					}
+					_relais_data[R_P1_RED].pwm = tmp;
+
+					if (_dmx_data[i] & D1_STROBE)
+					{
+						if ((_dmx_data[i] & D1_STROBE_10) == D1_STROBE_10)
+						{
+							tmp = STROBE_FAST;
+						}
+						else if ((_dmx_data[i] & D1_STROBE_4) == D1_STROBE_4)
+						{
+							tmp = STROBE_MID;
+						}
+						else if ((_dmx_data[i] & D1_STROBE_1) == D1_STROBE_1)
+						{
+							tmp = STROBE_SLOW;
+						}
+						_strobe_count[R_P1_RED] = 0;
+						_relais_data[R_P1_RED].strobe = tmp;
+					}
+					else
+					{
+						_relais_data[R_P1_RED].strobe = STROBE_OFF;
+					}
+				}
+				else
+				{
+					_relais_data[R_P1_RED].pwm = PWM_OFF;
+				}
+
+				if (_dmx_data[i] & D2_POWER)
+				{
+					if ((_dmx_data[i] & D2_POWER_100) == D2_POWER_100)
+					{
+						tmp = PWM_FULL;
+					}
+					else if ((_dmx_data[i] & D2_POWER_60) == D2_POWER_60)
+					{
+						tmp = PWM_HALF;
+					} else if ((_dmx_data[i] & D2_POWER_30) == D2_POWER_30)
+					{
+						tmp = PWM_MIN;
+					}
+					_relais_data[R_P2_WHITE].pwm = tmp;
+
+					if (_dmx_data[i] & D2_STROBE)
+					{
+						if ((_dmx_data[i] & D2_STROBE_10) == D2_STROBE_10)
+						{
+							tmp = STROBE_FAST;
+						}
+						else if ((_dmx_data[i] & D2_STROBE_4) == D2_STROBE_4)
+						{
+							tmp = STROBE_MID;
+						}
+						else if ((_dmx_data[i] & D2_STROBE_1) == D2_STROBE_1)
+						{
+							tmp = STROBE_SLOW;
+						}
+						_strobe_count[R_P2_WHITE] = 0;
+						_relais_data[R_P2_WHITE].strobe = tmp;
+					}
+					else
+					{
+						_relais_data[R_P2_WHITE].strobe = STROBE_OFF;
+					}
+				}
+				else
+				{
+					_relais_data[R_P2_WHITE].pwm = PWM_OFF;
+				}
+			}
+			_dmx_old_data[i] = _dmx_data[i];
+		}
+	}
+}
+
+void init_uart()
+{
+	uint8_t tmp;
+
+	UBRRH = UBRR_VAL >> 8;
+	UBRRL = UBRR_VAL & 0xFF;
+
+	do
+	{
+		tmp = UDR;
+	}
+	while (UCSRA & (1 << RXC));
+	UDR = 0;
+	UCSRA = 0;
+	UCSRB = (1 << RXCIE) | (1 << RXEN);
+	UCSRC = (3 << UCSZ0) | (1 << USBS);
+}
+
+void init_timer()
+{
+	TCCR1B = 1;
+	OCR1A = 0;
+
+	TIMSK |= (1 << OCIE1A);
+}
+
+inline void init_gpio() {
+	DDRB |= (R_MASK);
+	DDRD |= (LED_PIN);
+}
+
+inline void init_relais()
+{
+	uint8_t i = 0;
+	_relais_data[R_P1_RED] = (relais_data_t) {0, 0, 0, R1_PIN};
+	_relais_data[R_P2_WHITE] = (relais_data_t) {0, 0, 0, R2_PIN};
+	for(i = 2; i < NUM_RELAIS; i++ )
+	{
+		_relais_data[i] = (relais_data_t) {0,0,0,0};
+	}
+}
+
+inline void init_dmx()
+{
+	uint8_t i = 0;
+	for (i = 0; i < DMX_NUM_CHANNELS; i++)
+	{
+		_dmx_data[i] = 0;
+		_dmx_old_data[i] = 0;
+	}
+}
+
+inline void standalone_fallback()
+{
+	_relais_data[R_P1_RED].pwm = PWM_FULL;
+	_relais_data[R_P1_RED].strobe = STROBE_OFF;
+	_relais_data[R_P2_WHITE].pwm = PWM_FULL;
+	_relais_data[R_P2_WHITE].strobe = STROBE_OFF;
+}
+
+int main(void)
+{
+	init_gpio();
+	init_uart();
+	init_relais();
+	init_dmx();
+	init_timer();
+
+	standalone_fallback();
+
+	sei();
+
+	for (;;)
+	{
+		if (_flag)
+		{
+			processData();
+			_flag = 0;
+		}
+		if (_led_latency > 0)
+		{
+			_led_latency--;
+			SET_LED;
+		}
+		else
+		{
+			CLEAR_LED;
+		}
+	}
+
+	return 0;
+}
---
+++ DMX_dosencontroller.h
@@ -0,0 +1,97 @@
+#include "avr/io.h"
+#include <stdio.h>
+#include <string.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+
+#define DOSE_CHANNEL 	46
+
+#define D1_POWER		0x03
+#define D1_POWER_0		0x00
+#define D1_POWER_30		0x01
+#define	D1_POWER_60		0x02
+#define D1_POWER_100	0x03
+
+#define D1_STROBE		0x0C
+#define D1_STROBE_0		0x00
+#define D1_STROBE_1		0x04
+#define D1_STROBE_4		0x08
+#define D1_STROBE_10	0x0C
+
+#define D2_POWER		( D1_POWER << DOSE_CHANNEL )
+#define D2_POWER_0		( D1_POWER_0 << DOSE_CHANNEL )
+#define D2_POWER_30		( D1_POWER_30 << DOSE_CHANNEL )
+#define	D2_POWER_60		( D1_POWER_60 << DOSE_CHANNEL )
+#define D2_POWER_100	( D1_POWER_100 << DOSE_CHANNEL )
+
+#define D2_STROBE		( D1_STROBE << DOSE_CHANNEL )
+#define D2_STROBE_0		( D1_STROBE_0 << DOSE_CHANNEL )
+#define D2_STROBE_1		( D1_STROBE_1 << DOSE_CHANNEL )
+#define D2_STROBE_4		( D1_STROBE_4 << DOSE_CHANNEL )
+#define D2_STROBE_10	( D1_STROBE_10 << DOSE_CHANNEL )
+
+#define STROBE_OFF		0
+#define STROBE_SLOW		30
+#define STROBE_MID		15
+#define STROBE_FAST		5
+
+// Relais
+#define NUM_RELAIS	5
+#define R_PORT PORTB
+#define R1_PIN (1<<PORTB0)
+#define R2_PIN (1<<PORTB1)
+#define R3_PIN (1<<PORTB2)
+#define R4_PIN (1<<PORTB3)
+#define R5_PIN (1<<PORTB4)
+#define R_MASK ( R1_PIN | R2_PIN | R3_PIN | R4_PIN | R5_PIN )
+#define CLEAR_ALL 	(PORTB &= ~R_MASK)
+
+#define R_P1_RED	0
+#define R_P2_WHITE	1
+
+
+// LEDs
+#define LED_PIN 					(1<<PORTD6)
+#define SET_LED 	(PORTD |= LED_PIN)
+#define CLEAR_LED 	(PORTD &= ~LED_PIN)
+
+// PWM Settings
+#define F_PWM 				100   	// PWM-Frequenz in Hz
+#define PWM_STEPS 			200		// PWM-Schritte pro Zyklus(1..256)
+
+#define FLASH_CNT			255
+
+
+// LED settings
+#define PWM_OFF			0
+#define PWM_FULL		PWM_STEPS
+#define PWM_HALF		(PWM_STEPS>>2)
+#define PWM_QUART		(PWM_STEPS>>3)
+#define PWM_MIN			(PWM_STEPS>>4)
+
+#define LED_LATENCY 10000
+
+struct relais_data {
+	uint8_t pwm;
+	uint8_t strobe;
+	uint8_t enable;
+	uint8_t PIN;
+};
+
+typedef struct relais_data relais_data_t;
+
+// UART
+#define BAUD 250000UL
+#define UBRR_VAL ((F_CPU+BAUD*8)/(BAUD*16)-1)
+#define BAUD_REAL (F_CPU/(16*(UBRR_VAL+1)))
+#define BAUD_ERROR ((BAUD_REAL*1000)/BAUD)
+ #if ((BAUD_ERROR<990) || (BAUD_ERROR>1010))
+	#error Systematischer Fehler der Baudrate grösser 1% und damit zu hoch!
+#endif
+
+
+// PWM
+#define T_PWM 					(F_CPU/(F_PWM*PWM_STEPS))
+#if (T_PWM<(93+5))
+    #error T_PWM zu klein, F_CPU muss vergrösst werden oder F_PWM oder PWM_STEPS verkleinert werden
+#endif
